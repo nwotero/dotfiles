cc5cb273df4a7aaf245307da29aa7d5be5e5619d
Nicholas Otero <notero@red-shift.tech>
2021-11-11 22:56:36 -0500

    Add bashrc_ext to bashrc with setup script.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 nvim_dir=$HOME/.config/nvim/
 
-[[ -a $HOME/bashrc_ext ]] || ln -s $dir/bashrc_ext $HOME/bashrc_ext
+[[ -a $HOME/bashrc_ext ]] || ln -s $dir/bashrc_ext $HOME/bashrc_ext && echo 'source $HOME/bashrc_ext' >> $HOME/.bashrc
 [[ -f $nvim_dir/init.vim ]] && mv $nvim_dir/init.vim $nvim_dir/_init.vim
 [[ -a $nvim_dir/init.vim ]] || ln -s $dir/init.vim $nvim_dir/init.vim
 
